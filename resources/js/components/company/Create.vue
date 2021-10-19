a638c2a966a443fd650475261aaa073b3a2144d0
Lindrit Krasniqi <linndritks@gmail.com>
2021-10-19 18:36:54 +0200

    Create company components index, edit, create

---
+++ resources/js/components/company/Create.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="mt-10 sm:mt-0">
+    <div class="md:grid md:grid-cols-3 md:gap-6">
+      <div class="md:col-span-1">
+        <div class="px-4 sm:px-0">
+          <h3 class="text-lg font-medium leading-6 text-gray-900">
+            Company Information - Create
+          </h3>
+          <p class="mt-1 text-sm text-gray-600">
+            Use a permanent address where you can receive mail.
+          </p>
+        </div>
+      </div>
+      <div class="mt-5 md:mt-0 md:col-span-2">
+        <partials-company :company="{}" :errors="errors" @submited="store" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PartialsCompany from "../partials/forms/company.vue";
+import { storeCompany } from "../../compositions/company";
+
+export default {
+  data: () => ({ errors: [] }),
+
+  components: {
+    PartialsCompany,
+  },
+
+  methods: {
+    store(company) {
+      this.errors = [];
+      storeCompany(company)
+        .then(() => this.$router.push({ name: "companies" }))
+        .catch((e) => (this.errors = e.response.data.errors));
+    },
+  },
+};
+</script>
