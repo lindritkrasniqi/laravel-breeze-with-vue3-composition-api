cb780567a4ff442fc46c38901bffe0fd3feb5389
Lindrit Krasniqi <linndritks@gmail.com>
2021-10-25 11:41:20 +0200

    Update app

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,4 @@ require("alpinejs");
 import { createApp } from "vue";
 import routes from "./routes/";
 
-Vue.config.devtools = false;
-Vue.config.debug = false;
-Vue.config.silent = true;
-
 createApp({}).use(routes).mount("#app");
